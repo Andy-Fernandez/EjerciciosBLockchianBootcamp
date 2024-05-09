96af9080f158d50d83cfbdd7acbb403938c93cfc
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2024-05-08 21:48:48 -0400

    ejecicio2 done

--- contracts/ejercicios/Ejercicio_2.sol
+++ contracts/ejercicios/Ejercicio_2.sol
@@ -44,6 +44,49 @@ pragma solidity 0.8.24;
  * - npx hardhat test test/EjercicioTesting_2.js
  */
 
+// contract Ejercicio_2 {
+//     // 1
+//     // definir un 'admin'
+//     // no cambiar
+//     address public admin = 0x08Fb288FcC281969A0BBE6773857F99360f2Ca06;
+
+//     modifier soloAdmin() {
+//         // definir logica
+//         _;
+//     }
+
+//     // function metodoAccesoProtegido() {
+//     //     // ...logica
+//     // }
+
+//     // 2
+//     // definir lista blanca con un mapping
+//     // mapping listaBlanca;
+//     // modifier soloListaBlanca
+
+//     // function metodoPermisoProtegido
+
+//     // function incluirEnListaBlanca
+
+//     // 3
+//     // definir un rango de tiempo cualquiera (e.g. hoy + 30 days)
+//     // En solidity se cumple que: 1 days = 86400 seconds
+//     uint256 public tiempoLimite = block.timestamp + 30 days;
+
+//     // modifier soloEnTiempo
+
+//     // function metodoTiempoProtegido
+
+//     // 4
+//     // definir un booleano para pausar
+//     // bool public pausado;
+//     // modifier pausa
+
+//     // function metodoPausaProtegido
+
+//     // function cambiarPausa()
+// }
+
 contract Ejercicio_2 {
     address public admin = 0x08Fb288FcC281969A0BBE6773857F99360f2Ca06;
     mapping(address => bool) public whitelist;
@@ -92,8 +135,8 @@ contract Ejercicio_2 {
         emit RemovedFromWhitelist(_account);
     }
 
-    function cambiarPausa(bool _nuevoEstado) public soloAdmin {
-    paused = _nuevoEstado;
+    function cambiarPausa() public soloAdmin {
+    paused = !paused;
 }
 
 
